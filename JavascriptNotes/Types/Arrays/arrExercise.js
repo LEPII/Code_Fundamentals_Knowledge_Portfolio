f9e96f5bbf53336bbd92a5a33ca452497658f0b0
Luis Perez <lperezdev843@gmail.com>
2024-12-06 23:38:55 -0500

    a few array exercises

--- JavascriptNotes/Types/Arrays/arrExercise.js
+++ JavascriptNotes/Types/Arrays/arrExercise.js
@@ -68,22 +68,41 @@ const countOccurrences = (array, searchElement) => {
   }
 };
 
-// 6. Get Max
+// 6. Get Max == Review!!!
 
 const allNumbs = [1, 2, 3, 4];
 
 const max = getMax(allNumbs);
 
 const getMax = (array) => {
-  let max = 0;
+  if (array.length === 0) return undefined;
 
-  for (number of array) {
-    if (array.length === 0) {
-      return undefined;
-    } else if (number > max) {
-      max = number;
-      return max;
-    }
-    return max;
-  }
+  // let max = array[0];
+
+  // for (let i = 1; 1 < array.length; i++) if (array[i] > max) max = array[i];
+  // return max;
+
+  // OR
+
+  return array.reduce((a, b) => (a > b ? a : b));
 };
+
+// 7. Movies
+
+const movies = [
+  { title: "a", year: 2018, rating: 4.5 },
+  { title: "b", year: 2018, rating: 4.7 },
+  { title: "c", year: 2018, rating: 3 },
+  { title: "d", year: 2017, rating: 4.5 },
+];
+
+// All the movies in 2018 with rating > 4
+// Sort them by their rating
+// Descending Order
+// Pick their title
+
+movies
+  .filter((m) => m.year === 2018 && m.rating >= 4)
+  .sort((a, b) => a.rating - b.rating)
+  .reverse()
+  .map((m) => m.title);
