6a51c4496e46d3b85a56c6129014d9619b95c39d
Luis Perez <lperezdev843@gmail.com>
2025-01-16 23:54:43 -0500

    getters and setters

--- JavascriptNotes/Advanced/miscellaneous.js
+++ JavascriptNotes/Advanced/miscellaneous.js
@@ -3,6 +3,8 @@
 //// Table Of Content
 
 //// Computed Property Names
+//// Getters And Setters
+
 //// Optional Chaining
 //// Short Circuiting
 //// Type Casting (Type Conversion vs Coercion)
@@ -30,3 +32,42 @@ const obj = {
 // - Dynamic Object Creation: You can create objects with property names that are determined at runtime.
 // - Flexibility: Easily adapt object structures based on variables, functions, or other dynamic values.
 // - Improved Code Readability: In some cases, computed property names can make your code more concise and easier to understand.
+
+//// Getters And Setters ////
+
+// In JavaScript, getters and setters are special methods that provide a controlled way to access and modify the values of an object's properties.
+
+// Getter: A method that retrieves the value of a property. It's invoked when you try to read the value of a property.
+
+// Setter: A method that sets the value of a property. It's invoked when you try to assign a value to a property.
+
+// Before ES6 (ECMAScript 2015), there was no direct syntax for getters and setters. Developers had to simulate them using the following methods:
+// - Using Closure
+// - Using Object.defineProperty()
+
+// ES6 introduced a much cleaner and more concise syntax for defining getters and setters:
+
+// Key Advantages of Getters and Setters
+
+class Puppy {
+  constructor(pup) {
+    this._pup = pup;
+  }
+  get pup() {
+    return this._pup;
+  }
+
+  set pupNickName(newNickName) {
+    this._pup = newNickName.toUpperCase();
+  }
+}
+
+const puppy = new Puppy("Lily")
+
+puppy.pup // ==> Lily
+puppy.pupNickName = "Patico<3" // => PATICO<3
+
+// Encapsulation: Hide the internal implementation details of how a property is stored or calculated.
+// Data Validation: Enforce constraints on the values that can be assigned to a property.
+// Computed Properties: Calculate property values based on other properties or internal state.
+// Improved Maintainability: Make code more modular and easier to understand and modify.
