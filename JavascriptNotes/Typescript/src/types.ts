77d8a938331b4eea67cfba011c79fc86c5a8c1c3
Luis Perez <lperezdev843@gmail.com>
2025-01-24 23:33:23 -0500

    more detailed notes on typescript basic types

--- JavascriptNotes/Typescript/src/types.ts
+++ JavascriptNotes/Typescript/src/types.ts
@@ -3,17 +3,27 @@
 //// Table Of Content
 
 //// - General
+//// - Built In Type
 //// - The Any Type
 //// - The Array Type
-//// - The Tuples Type
 //// - The Enums Type
 //// - The Functions Type
 //// - The Objects Type
+//// - The Tuples Type
 
 //// General ////
 
 // - Type Inference - TypeScript often infers the type of a variable based on its initial value, reducing the need for explicit type annotations.
 // - Type Compatibility - TypeScript has rules for determining when two types are compatible. For example, a variable of type number can be assigned to a variable of type number | string.
+
+//// Built In Primitive Types ////
+
+// boolean;
+// null;
+// number;
+// string
+// symbol
+// undefined
 
 //// The Any Type ////
 
@@ -32,15 +42,12 @@ let numbers: number[] = [1, 2, 3];
 let names: string[] = ["Alice", "Bob", "Charlie"];
 let mixed: (number | string)[] = [1, "two", 3];
 
-//// The Tuples Type ////
-
-// Tuple - A fixed-length array with elements of specific types.
-let person: [string, number] = ["Lui", 30];
-// Access elements by index: person[0] // "John"
-
 //// The Enums Type ////
 
-// Enum - Defines a set of named constants.
+// Enum - Defines a set of related named constants.They provide a more readable and maintainable way to represent a fixed set of values.
+// By default, enum members are assigned numerical values starting from 0.
+// Direction.Up will have the value 0, Direction.Down will have the value 1, and so on.
+
 enum Direction {
   Up,
   Down,
@@ -48,7 +55,17 @@ enum Direction {
   Right,
 }
 
-// Direction.Up will have the value 0, Direction.Down will have the value 1, and so on.
+// You can assign custom values to individual enum members:
+
+enum StatusCode {
+  Success = 200,
+  NotFound = 404,
+  ServerError = 500,
+}
+
+let myStatusCode: StatusCode = StatusCode.NotFound;
+
+console.log(myStatusCode); // => 404
 
 //// The Functions Type ////
 
@@ -67,3 +84,16 @@ interface Person {
 }
 
 const person2: Person = { name: "Lui", age: 26 };
+
+//// The Tuples Type ////
+
+// Tuple - A fixed-length array with elements of specific types.
+let person: [string, number] = ["Lui", 30];
+// Access elements by index: person[0] // "John"
+
+// You can make elements in a tuple optional by using the ? symbol
+let mySecondTuple: [string, number?] = ["hello"]; // Second element is optional
+
+// Rest Elements:
+// You can use the rest operator (...) to represent an array of elements after a fixed number of elements:
+let myThirdTuple: [string, ...number[]] = ["Yo", 1, 2, 3];
