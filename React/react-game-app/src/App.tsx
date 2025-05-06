34c5ef2b1d44f12d93d2f350e28ce279f20ed971
Luis Perez <lperezdev843@gmail.com>
2025-05-05 23:13:36 -0400

    react project set up

---
+++ React/react-game-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
