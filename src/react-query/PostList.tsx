8eb0ab515002d1da59115ae998afeeecf16b291f
Luis Perez <lperezdev843@gmail.com>
2026-01-22 23:34:25 -0500

    infinite query tanstack notes

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { usePosts } from "./hooks/usePost";
+import React from "react";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
+  // const [userId, setUserId] = useState<number>();
   const pageSize = 10;
   const [page, setPage] = useState(1);
 
   // here we pass a query object. This has nothing to do with react query, it's a common design pattern.
   // A query object is an object that contains all of the value for querying a set of objects.
-  const { data, error, isLoading } = usePosts({ page, pageSize });
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
   if (isLoading) return <p>hol up hol up</p>;
 
@@ -16,7 +18,7 @@ const PostList = () => {
 
   return (
     <>
-      <select
+      {/* <select
         onChange={(event) => setUserId(parseInt(event.target.value))}
         value={userId}
         className="form-select mb-3">
@@ -24,24 +26,23 @@ const PostList = () => {
         <option value="1">User 1</option>
         <option value="2">User 2</option>
         <option value="3">User 3</option>
-      </select>
+      </select> */}
       <ul className="list-group">
-        {data?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
       <button
-        disabled={page == 1}
-        className="btn btn-primary my-3"
-        onClick={() => setPage(page - 1)}>
-        Previous
-      </button>
-      <button
         className="btn btn-primary my-3 ms-3"
-        onClick={() => setPage(page + 1)}>
-        Next{" "}
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}>
+        {isFetchingNextPage ? "Loading..." : "LoadMore"}
       </button>
     </>
   );
--- src/react-query/hooks/usePost.ts
+++ src/react-query/hooks/usePost.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,27 +9,31 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-  const fetchPosts = () =>
+  const fetchPosts = ({ pageParam = 1}) =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
         params: {
-          _start: (query.page - 1) * query.pageSize,
+          _start: (pageParam - 1) * query.pageSize,
           _limit: query.pageSize,
         },
       })
       .then((res) => res.data);
 
-  return useQuery<Post[], Error>({
+  // When using an infinite query, we cannot use a state variable to keep track of the page number because infiniteQuery handle pagination automatically.
+  return useInfiniteQuery<Post[], Error>({
     // The query is a parameter for this query. Every time the value of the query changes, react query will fetch the posts for that user from the backend. Very similar to the dependency hook in the useEffect hook.
     queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 10 * 1000,
     keepPreviousData: true,
+    // to keep track of the page number, InfiniteQuery have a function for that.
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 };
 
